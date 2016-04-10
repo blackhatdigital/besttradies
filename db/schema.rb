10c4292a6f9ccfb3198f95a8a0127fe27709e0c1
Blackhat Digital <josh@blackhatdigital.com.au>
2016-04-09 23:37:58 -0700

    Add Name to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160410061155) do
+ActiveRecord::Schema.define(version: 20160410063708) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 20160410061155) do
     t.boolean  "whitecard"
     t.string   "abn"
     t.decimal  "price"
+    t.string   "firstname"
+    t.string   "lastname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
