30ae327352defa1a8ac66d27d7b538d24a2bf454
Blackhat Digital <josh@blackhatdigital.com.au>
2016-04-22 14:13:24 -0700

    Add Controllers

--- db/migrate/20160422205342_create_proposals.rb
+++
@@ -1,8 +0,0 @@
-class CreateProposals < ActiveRecord::Migration
-  def change
-    create_table :proposals do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/20160422205414_create_skills.rb
+++
@@ -1,8 +0,0 @@
-class CreateSkills < ActiveRecord::Migration
-  def change
-    create_table :skills do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
