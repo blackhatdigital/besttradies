e9fbca81ac1f249d48de932a5145b130dca9f02c
Blackhat Digital <josh@blackhatdigital.com.au>
2016-07-12 09:25:00 +1000

    update devise sanitiser in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,45 +15,10 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
 
 
-    devise_parameter_sanitizer.permit(:sign_up) << :avatar
-    devise_parameter_sanitizer.permit(:sign_up) << :category
-    devise_parameter_sanitizer.permit(:sign_up) << :is_tradie
-    devise_parameter_sanitizer.permit(:sign_up) << :company
-    devise_parameter_sanitizer.permit(:sign_up) << :phone
-    devise_parameter_sanitizer.permit(:sign_up) << :firstname
-    devise_parameter_sanitizer.permit(:sign_up) << :lastname
-    devise_parameter_sanitizer.permit(:sign_up) << :dob
-    devise_parameter_sanitizer.permit(:sign_up) << :bio
-    devise_parameter_sanitizer.permit(:sign_up) << :skills
-    devise_parameter_sanitizer.permit(:sign_up) << :address
-    devise_parameter_sanitizer.permit(:sign_up) << :city
-    devise_parameter_sanitizer.permit(:sign_up) << :state
-    devise_parameter_sanitizer.permit(:sign_up) << :postcode
-    devise_parameter_sanitizer.permit(:sign_up) << :car
-    devise_parameter_sanitizer.permit(:sign_up) << :equipment
-    devise_parameter_sanitizer.permit(:sign_up) << :whitecard
-    devise_parameter_sanitizer.permit(:sign_up) << :abn
-    devise_parameter_sanitizer.permit(:sign_up) << :price
+devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar, :category, :is_tradie, :company, :phone, :firstname, :lastname, :dob, :bio, :skills, :address, :city, :sate, :postcode, :car, :equipment, :whitecard, :abn, :price ])
+devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :category, :is_tradie, :company, :phone, :firstname, :lastname, :dob, :bio, :skills, :address, :city, :sate, :postcode, :car, :equipment, :whitecard, :abn, :price ])
 
-    devise_parameter_sanitizer.permit(:account_update) << :avatar
-    devise_parameter_sanitizer.permit(:account_update) << :category
-    devise_parameter_sanitizer.permit(:account_update) << :company
-    devise_parameter_sanitizer.permit(:account_update) << :phone
-    devise_parameter_sanitizer.permit(:account_update) << :firstname
-    devise_parameter_sanitizer.permit(:account_update) << :lastname
-    devise_parameter_sanitizer.permit(:account_update) << :dob
-    devise_parameter_sanitizer.permit(:account_update) << :bio
-    devise_parameter_sanitizer.permit(:account_update) << :skills
-    devise_parameter_sanitizer.permit(:account_update) << :address
-    devise_parameter_sanitizer.permit(:account_update) << :city
-    devise_parameter_sanitizer.permit(:account_update) << :state
-    devise_parameter_sanitizer.permit(:account_update) << :postcode
-    devise_parameter_sanitizer.permit(:account_update) << :car
-    devise_parameter_sanitizer.permit(:account_update) << :equipment
-    devise_parameter_sanitizer.permit(:account_update) << :whitecard
-    devise_parameter_sanitizer.permit(:account_update) << :abn
-    devise_parameter_sanitizer.permit(:account_update) << :price
-
+  
   end
 
   
